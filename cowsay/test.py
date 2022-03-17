241eb26a4948992abd1ddad9757d03ed08bd2dc4
Aditya-aot <aditya.aot.11@gmail.com>
2022-03-17 11:10:01 +0530

    testing COWSAY

---
+++ cowsay/test.py
@@ -0,0 +1,15 @@
+import cowsay
+cowsay.daemon("Hello")
+
+cowsay.cow("Hello")
+
+cowsay.trex("Hello")
+
+cowsay.dragon("Hello")
+
+cowsay.ghostbusters("Hello")
+# print(cowsay.get_output_string('trex', 'Hello (extinct) World'))
+
+
+
+print(cowsay.char_names)
